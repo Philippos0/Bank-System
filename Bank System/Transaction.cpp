539ddd9a6eaa51fbe21912207a154234ec0d3893
Philippos0 <128741085+Philippos0@users.noreply.github.com>
2024-10-31 21:58:48 +0200

    Delete Bank System directory

--- Bank System/Transaction.cpp
+++
@@ -1,9 +0,0 @@
-#include "Transaction.h"
-#include <iostream>
-
-Transaction::Transaction(const std::string& type, double amount)
-    : type(type), amount(amount) {}
-
-void Transaction::display() const {
-    std::cout << "Transaction: " << type << ", Amount: " << amount << std::endl;
-}
